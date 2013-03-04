da98476c9aec9b0d26b00ea6de99dc7b3b530a77
Peter Wong <peter1wong@gmail.com>
2013-03-04 23:01:15 +1300

    revision

---
+++ js/base.js
@@ -0,0 +1,35 @@
+// Install app
+if (navigator.mozApps) {
+    var checkIfInstalled = navigator.mozApps.getSelf();
+    checkIfInstalled.onsuccess = function () {
+        if (checkIfInstalled.result) {
+            // Already installed
+        }
+        else {
+            var install = document.querySelector("#install"),
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/package.manifest";
+                // alert(manifestURL);
+            install.parentNode.className = "show-install";
+            install.onclick = function () {
+                var installApp = navigator.mozApps.installPackage("http://localhost/Firefox-OS-Boilerplate-App/package.manifest");
+                installApp.onsuccess = function(data) {
+                    install.style.display = "none";
+                };
+                installApp.onerror = function() {
+                    alert("Install failed\n\n:" + installApp.error.name);
+                };
+            };
+        }
+    };
+}
+else {
+    console.log("Open Web Apps not supported");
+}
+
+// Reload content
+var reload = document.querySelector("#reload");
+if (reload) {
+    reload.onclick = function () {
+        location.reload(true);
+    };
+}
