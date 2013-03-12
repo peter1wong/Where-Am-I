d7e7a7800ed0ee715bebdf8c8d65f5eeb8da286c
Peter Wong <peter1wong@gmail.com>
2013-03-12 16:39:29 +1300

    1

--- js/webapp.js
+++ js/webapp.js
@@ -31,13 +31,9 @@
 
     var sendSMS2 = document.querySelector("#send-sms2");
     if (sendSMS2) { 
-        sendSMS2.onclick = function () {
         	var sms2 = navigator.mozSMS;
-        	sms2.send("", "hello world");
-                }
-            });
-        }
-    }
+        	sms2.send("+123455", "hello world");
+            };
 
 
 
