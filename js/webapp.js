30da7959d48b211336d23c70256db1d7208fa1a0
Peter Wong <peter1wong@gmail.com>
2013-03-24 21:06:10 +1300

    Update webapp.js

--- js/webapp.js
+++ js/webapp.js
@@ -145,4 +145,37 @@ else {
 function process(){
 	alert("testing");
 }
-	
+
+
+
+
+var GiftModel = function(gifts) {
+    var self = this;
+    self.gifts = ko.observableArray(gifts);
+ 
+    self.addGift = function() {
+        self.gifts.push({
+            name: "",
+            price: ""
+        });
+    };
+ 
+    self.removeGift = function(gift) {
+        self.gifts.remove(gift);
+    };
+ 
+    self.save = function(form) {
+        alert("Could now transmit to server: " + ko.utils.stringifyJson(self.gifts));
+        // To actually transmit to server as a regular form post, write this: ko.utils.postJson($("form")[0], self.gifts);
+    };
+};
+ 
+var viewModel = new GiftModel([
+    { name: "Tall Hat", price: "39.95"},
+    { name: "Long Cloak", price: "120.00"}
+]);
+ko.applyBindings(viewModel);
+ 
+// Activate jQuery Validation
+$("form").validate({ submitHandler: viewModel.save });
+
